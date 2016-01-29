2a65781ee401357e35a00e0d9f4b6be69714f604
tammy41 <ty1695@qq.com>
2016-01-29 14:57:44 +0800

    my home page

---
+++ 我的主页/js/css.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function css_link1(){
+	var btn = documnent.getElementById=("link1");
+	if(btn.href == "css_one.css"){
+		btn.href="css_two.css";
+	}else{
+		btn.href="css_one.css";
+	}
+}
