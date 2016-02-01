34c190cee720017b224a2246ac4f497596e6c44b
tammy41 <ty1695@qq.com>
2016-02-01 22:00:27 +0800

    album

---
+++ 知识总结/MyHomepage/js/cd.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+function xianshi1(){
+	document.getElementById("cd1").style.display = "block";
+}
+function yincang1(){
+	document.getElementById("cd1").style.display = "none";
+}
+function xianshi2(){
+	document.getElementById("cd2").style.display = "block";
+}
+function yincang2(){
+	document.getElementById("cd2").style.display = "none";
+}
+function xianshi3(){
+	document.getElementById("cd3").style.display = "block";
+}
+function yincang3(){
+	document.getElementById("cd3").style.display = "none";
+}
